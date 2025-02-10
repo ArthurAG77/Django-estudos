32b0968af7c56c985380b8e78f6be18595546463
ArthurAG77 <arthur.ag77@gmail.com>
2025-02-10 15:58:46 -0300

    ADD TO NOTES

--- NOTES/uteis.md
+++
@@ -1,5 +0,0 @@
-# Utilidades de django aprendidas durante meu estudo
-
-Se executar o comando:<br>
-`django-admin --help`<br>
-nos retorna uma lista de comandos uteis do django
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200, blank=True, null=True)
+    brand = models.CharField(max_length=100, blank=True, null=True)
