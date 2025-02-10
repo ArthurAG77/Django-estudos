2e4543e2c484ce1fd26090f4dcc107fac82a01c5
ArthurAG77 <arthur.ag77@gmail.com>
2025-02-10 17:17:35 -0300

    some changes

--- cars/models.py
+++ cars/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
-    model = models.CharField(max_length=200, blank=True, null=True)
-    brand = models.CharField(max_length=100, blank=True, null=True)
+    model = models.CharField(max_length=200, blank=False, null=False)
+    brand = models.CharField(max_length=100, blank=False, null=False)
+    factory_year = models.IntegerField(blank=True, null=True) #trocar pra true true
+    model_year = models.IntegerField(blank=True, null=True) #trocar pra true true
+    value = models.FloatField(blank=True, null=True) #trocar pra true true
