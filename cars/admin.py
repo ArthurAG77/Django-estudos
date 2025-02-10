0bd7a3b547cef7d89c86330881fbd6160ceae91d
ArthurAG77 <arthur.ag77@gmail.com>
2025-02-10 18:38:05 -0300

    some changes

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 # Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', )
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value') # Campos que você quer que apareça no django admin
     search_fields = ('model', 'brand') # Em quais campos a pesquisa deve funcionar?
 
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
