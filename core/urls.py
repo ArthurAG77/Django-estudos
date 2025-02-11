86b823f9182739f9b92630b40cb32f24b4d8c303
ArthurAG77 <arthur.ag77@gmail.com>
2025-02-11 18:16:12 -0300

    added the url pointing to the first view

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import CarView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', CarView)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
