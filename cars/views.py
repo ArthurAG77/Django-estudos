581941d527c904496c4d7a84ff4599ec3d5e2503
ArthurAG77 <arthur.ag77@gmail.com>
2025-02-11 21:06:25 -0300

    commenting

--- cars/views.py
+++ cars/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def carview(request):
-    return render(request,'cars/cars.html') 
+    return render(request,'cars/cars.html')  # em settings.py o diretório de templates é BASE_DIR/templates então ele não enxerga diretamente o arquivo html, por isso adicionamos o nome do diretório antes do html
