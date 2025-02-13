a1577c1cf532b5258c021f55ecc4e302b242880a
ArthurAG77 <arthur.ag77@gmail.com>
2025-02-13 12:03:21 -0300

    changed the brand_name in brand model to be a unique field

--- cars/models.py
+++ cars/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200, blank=False, null=False)
+    name = models.CharField(max_length=200, blank=False, null=False, unique=True)
     
     def __str__(self):
         return self.name
