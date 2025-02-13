79bc6af80b565167cbdc7553effb63c789277b2c
ArthurAG77 <arthur.ag77@gmail.com>
2025-02-13 12:04:28 -0300

    playing with ORM

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from cars.models import Car
 # Create your views here.
 
 def carview(request):
-    return render(request,'cars/cars.html')  # em settings.py o diretório de templates é BASE_DIR/templates então ele não enxerga diretamente o arquivo html, por isso adicionamos o nome do diretório antes do html
+    cars = Car.objects.filter(brand__name__contains='fiat') # -> Seleciona todos os registros do banco de dados
+
+    return render(
+        request,'cars/cars.html',
+        {'cars': cars}
+        )  # em settings.py o diretório de templates é BASE_DIR/templates então ele não enxerga diretamente o arquivo html, por isso adicionamos o nome do diretório antes do html
